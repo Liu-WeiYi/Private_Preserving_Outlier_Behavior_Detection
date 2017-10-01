4709d12a572f5d550bd37dc33d05e8ceeed175c0
uniqueliu <weiyiliu@us.ibm.com>
2017-09-30 23:46:43 -0400

    Finish Com_based Abnormal Detection

--- Abnormal_Scenario_Generation.py
+++ Abnormal_Scenario_Generation.py
@@ -2,6 +2,8 @@
 # purpose: Define Different Types of Abnormal Scenario
 # Author: weiyiliu@us.ibm.com
 
+import networkx as nx
+
 from Algorithms.pylouvain import LouvainCommunities
 
 def add_different_Devices(current_user_time_based_device):
@@ -11,6 +13,7 @@ def add_different_Devices(current_user_time_based_device):
     @param current_user_time_based_device
     @return abnormal_embeded current_user_time_based_device
     """
+    pass
 
 
 def Detection_Outliers(network):
@@ -18,12 +21,39 @@ def Detection_Outliers(network):
     detect outliers in a network
 
     @param network: weighted/merged network from multilayer network
+
+    @return weight_list, weight_com
     """
+    modify_weight_network = nx.Graph()
+    for e in network.edges(data=True):
+        u,v,W = e
+        w=W['weight']
+        if w == 0:
+            modify_weight_network.add_edge(u,v,weight=1)
+        elif w == 1:
+           modify_weight_network.add_edge(u,v,weight=1)
+        else:
+            modify_weight_network.add_edge(u,v,weight=w/100)
 
-   # --------------------- #
-   # Community Detection
-   # --------------------- #
+        modify_weight_network.add_edge(u,v)
 
-   NodeCommunity, Modularity = LouvainCommunities(network)
-   print('Current Modularity:\t', Modularity)
-   print('Node Communities Info:\n', NodeCommunity)
+    # 1. get community result
+    NodeCommunity, Modularity = LouvainCommunities(modify_weight_network)
+    # 2. get community score
+    score_set  = set()
+    score_com   = {}
+    for com in NodeCommunity:
+        current_score = 0
+        for n1_idx in range(len(com)-1):
+            for n2_idx in range(n1_idx+1, len(com)):
+                n1 = com[n1_idx]
+                n2 = com[n2_idx]
+                if n2 in nx.neighbors(network, n1):
+                    weight = network.get_edge_data(n1,n2)['weight']
+                    current_score += weight
+        score_set.add(current_score)
+        if current_score not in score_com.keys():
+            score_com[current_score] = []
+        score_com[current_score].append(com)
+
+    return score_set, score_com
--- main.py
+++ main.py
@@ -1,221 +1,45 @@
 # coding: utf-8
 # author: weiyiliu@us.ibm.com
 
-from __future__ import print_function
-from __future__ import division
-
 import sys
-import pickle
 import os
+import glob
 import json
+import pickle
+import time
 import networkx as nx
-import traceback
-from datetime import datetime
+import matplotlib.pyplot as plt
+import Abnormal_Scenario_Generation as ASG
 
-from utils import *
-from Abnormal_Scenario_Generation import *
-
-def extract_time(filenames, TEST_FLAG=True):
+def main(normal_path, abnormal_path):
     """
-    Extract all users ReachTime and DataTime Information
+    main entry
 
-    @return all_user_id, all_user_statistic_file
+    @param normal_path
+    @param abnormal_path
     """
-    all_user_id         = []
-    all_user_time_info  = {}
+    normal_files = glob.glob(normal_path+'/*.gml')
+    abnormal_files = glob.glob(abnormal_path+'/*.gml')
 
-    all_user_id_file    = 'all_user_id.txt'
-    user_time_info_file = 'user_time_info.json'
+    # --------------------- #
+    # Process normal People
+    # --------------------- #
+    for file in abnormal_files[0:2]:
+        print('current file:\t', file)
+        network = nx.read_gml(file)
+        score_set, score_com = ASG.Detection_Outliers(network)
 
-    # for TEST USE ONLY
-    if TEST_FLAG is True:
-        all_user_id_file = '10_all_user_id.txt'
-        user_time_info_file = '10_user_time_info.json'
-    # END TEST USE
-
-    if os.path.exists(user_time_info_file) and os.path.exists(all_user_id_file):
-        with open(all_user_id_file,'r+') as f:
-            for line in f.readlines():
-                all_user_id.append(line.strip())
-
-        all_user_time_info = json.load(open(user_time_info_file))
-        print('--Done loading files:\t%s, %s'%(all_user_id_file, user_time_info_file))
-    else:
-        all_user_id, all_user_time_info = extract_user_time(filenames)
-
-    return all_user_id, all_user_time_info
-
-
-def extract_user_info(all_user_id, filenames, TEST_FLAG=True):
-    """
-    Extract All users info based on Time and corresponding devices
-
-    @param all_user_id: a list to store all users id
-    @return all_user_info : a dict
-    """
-    all_user_info_file = "all_user_info.json"
-
-    # for TEST USE ONLY
-    if TEST_FLAG is True:
-        all_user_info_file = '10_all_user_info.json'
-    # END TEST USE
-
-    if os.path.exists(all_user_info_file):
-        all_user_info = json.load(open(all_user_info_file))
-        print('--Done Loading Files:\t%s'%all_user_info_file)
-    else:
-        all_user_info = extract_user_Devices_info(all_user_id, filenames)
-
-    return all_user_info
-
-
-def analysis_Time(user, per_user_info):
-    """
-
-    @return median_deltaT, Dates_Interval_Dict
-    """
-    try:
-        Reach_time = per_user_info["Reach_Time"]
-        # Data_time = per_user_info["Data_Time"]
-        # all_time=sorted(Reach_time+Data_time)
-        all_time = sorted(list(set(sorted(Reach_time))))
-        median_deltaT = find_median_delta_T(all_time)
-    except Exception as exc:
-        print('Abnormal User ID: %s'%user)
-        # print(sys.exc_info()[0])
-        print(traceback.format_exc())
-        print(exc)
-
-    # use median Delta_T to determine Time Interval among dates
-    # print('--Current Median_Delta_T = %.2f'%median_deltaT)
-    Dates_Interval_Dict = generate_time_group(all_time, median_deltaT)
-
-    return median_deltaT, Dates_Interval_Dict
-
-def group_user_behavior_by_Dates_interval(Dates_Interval_Dict, current_user_info):
-    """
-    group users behavior by dates
-
-    @param Dates_interval_Dict
-    @param current_user_info: per_user_info_dict, contain 'Reach_Time' and 'Data_Time'
-    @return grouped_user_info_dict
-    """
-    grouped_user_info_dict = {}
-
-    for key in Dates_Interval_Dict.keys():
-        # 1. extract time group
-        time_group = Dates_Interval_Dict[key]
-        # 2. for each time group, find devices attributes
-        grouped_devices_dict = extract_devices_behavior_on_time_group(
-            time_group,
-            current_user_info
-        )
-        # 3. assign current grouped devices with current time_group
-        grouped_user_info_dict[key] = grouped_devices_dict
-
-    return grouped_user_info_dict
-
-def construct_graph(name, devices_info):
-    """
-    Calculate devices behavior based on time_group
-
-    @param name: current graph's name
-    @param devices_info: a dict to store related 'Keyword' and 'ReachTime' for each devices
-
-    @return graph: nx.Graph object
-    """
-    graph = nx.Graph(name=name)
-
-    devices_list = list(devices_info.keys())
-    graph.add_nodes_from(devices_list)
-
-    if len(devices_list) > 1:
-        for d1_idx in range(len(devices_list)-1):
-            for d2_idx in range(d1_idx+1, len(devices_list)):
-                d1 = devices_list[d1_idx]
-                d2 = devices_list[d2_idx]
-                edge_weight = calculate_devices_weight(d1,d2,devices_info)
-                graph.add_edge(d1, d2, weight=edge_weight)
-
-    return graph
+        # For DEBUG
+        for s in set(score_set):
+            print('score: ', s)
+            for com in score_com[s]:
+                print('\t',com)
+        print('\n')
+        # END DEBUG
 
 if __name__ == "__main__":
 
-    TEST_FLAG = True # Indicate we use WorkStation in Watson
+    normal_path = 'Normal_Example'
+    abnormal_path = 'Abnormal_Example'
 
-    try:
-        if sys.argv[1] == '-w':
-            TEST_FLAG = False
-    except:
-        pass
-
-    # On Mac
-    filenames = ['ijcai_device_encode_test_sample.csv','ijcai_cookie_encode_test_sample.csv']
-
-    # On Work Station
-    if TEST_FLAG is False:
-        filenames = [
-            'ijcai_device_encode_training.csv',
-            'ijcai_device_encode_test.csv',
-            'ijcai_cookie_encode_training.csv',
-            'ijcai_cookie_encode_test.csv'
-        ]
-
-    """ 1. Extract All Users ReachTime and Data_Time """
-    # --- 1.1 Extract All User and corresponding ReachTime and DataTime --- #
-    all_user_id, all_user_time_info = extract_time(filenames, TEST_FLAG)
-
-    # --- 1.2 Extract All users info based on Time and corresponding devices --- #
-    all_user_info = extract_user_info(all_user_id, filenames, TEST_FLAG)
-
-    current_time = str(datetime.now()).split()[0] + '-' + str(datetime.now()).split()[1]
-    # --- Let's FLY!!!! --- #
-    for user_idx in range(len(all_user_id)):
-        # --- add Progress Statue --- #
-        percentage = 100*(user_idx+1)/len(all_user_id)
-        # print('\r>> Processing Users............ %.2f %%'%percentage,flush=True)
-        sys.stdout.write('\r>> Processing Users............ %.2f %%'%percentage)
-        sys.stdout.flush()
-        # --- End Progree Statue --- #
-        user = all_user_id[user_idx]
-
-        per_user_info = all_user_time_info[user]
-
-        # --- 2.1 Analysis Reach_time and Data_Time --- #
-        median_T, Dates_Interval_Dict = analysis_Time(user, per_user_info)
-        if Dates_Interval_Dict == {} and median_T == []:
-            print('Cannot Parse Current User: %s'%user)
-            continue
-
-        # --- 2.2 Extract User Devices Behaviors based on Dates Interval --- #
-        current_user_info = all_user_info[user]
-        current_user_time_based_device = group_user_behavior_by_Dates_interval(Dates_Interval_Dict, current_user_info)
-
-        # --- 2.3 Calculate Devices Related Similarity per time_group --- #
-        graph_list = []
-        for key in current_user_time_based_device.keys():
-            graph = construct_graph(
-                name=user+'_'+str(key),
-                devices_info=current_user_time_based_device[key]
-            )
-            graph_list.append(graph)
-
-        # --- 2.4 Create Multi-Layer Graph based on graph_list --- #
-        multi_layer_graph = merged_graph(user, graph_list)
-
-        # --- 2.5 Draw graph --- #
-        # nx.draw_networkx(multi_layer_graph)
-        draw_graph(str(median_T),current_time, str(user_idx), user, multi_layer_graph, self_define_pos=True,save_to_disk=True)
-
-        """
-        # ================================ #
-        # -- Create Date: 2017.09.27
-        # -- UPDATED: Introduce Abnormality ...
-        # ================================ #
-        """
-        # Abnormal Type 1:
-        # --- Add Different Devices --- #
-
-
-
-    print('\nall down!!!')
+    main(normal_path, abnormal_path)
