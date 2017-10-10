3688cc8c9e75ba4c59311e1c36e534d42bc15d72
uniqueliu <weiyiliu@us.ibm.com>
2017-10-10 13:07:38 -0400

    add abnormal detection

--- Abnormal_Scenario_Generation.py
+++ Abnormal_Scenario_Generation.py
@@ -3,8 +3,41 @@
 # Author: weiyiliu@us.ibm.com
 
 import networkx as nx
+import glob
+import random
+import copy
+
 
 from Algorithms.pylouvain import LouvainCommunities
+
+
+def _synthetic_network(syntheticFlag, network, devices_number):
+    """
+    add synthetic nodes to network
+
+    @param syntheticFlag
+    @param network
+    @param devices_number
+
+    return modified network
+    """
+    abnormal_network = copy.deepcopy(network)
+    if syntheticFlag == "add_edges":
+        # randomly add edges with abnormal device
+        node = "abnormal_device"
+        if len(network.nodes()) >= devices_number:
+            devices = random.sample(network.nodes(), devices_number)
+        else:
+            devices = random.sample(network.nodes(), 1)
+        for device in devices:
+            weight = random.uniform(0,1)
+            abnormal_network.add_weighted_edges_from([(node, device, weight)])
+    elif syntheticFlag == "add_isolated_nodes":
+        node = "abnormal_device"
+        abnormal_network.add_node(node)
+
+    return abnormal_network
+
 
 
 def Detection_Outliers(network):
@@ -26,10 +59,13 @@ def Detection_Outliers(network):
         else:
             modify_weight_network.add_edge(u,v,weight=w/100)
 
-        modify_weight_network.add_edge(u,v)
-
     # 1. get community result
     NodeCommunity, Modularity = LouvainCommunities(modify_weight_network)
+    """FIX BUGS: Louvain Community Algorithm will omit isolated node"""
+    ISO_Nodes = nx.isolates(network)
+    if len(ISO_Nodes) != 0:
+        for node in ISO_Nodes:
+            NodeCommunity.append([node])
     # 2. get community score
     score_set  = set()
     score_com   = {}
@@ -48,3 +84,75 @@ def Detection_Outliers(network):
         score_com[current_score].append(com)
 
     return score_set, score_com
+
+def synthetic_nodes(normal_path, synthetic_type):
+    """
+    add new nodes, try to identify if the algorithm can detect it.
+
+    @param normal_files: files that is normal
+    @return precision, Recall, F1_score
+    """
+    Precision  = []
+    Recall     = []
+    F1         = []
+
+    # all files
+    networks = []
+    normal_files = glob.glob(normal_path+'/*.gml')
+    networks = [nx.read_gml(file) for file in normal_files]
+
+    # Define How Many Times
+    synthetic_times = 100
+    # Define How Many Devices
+    devices_number = 2
+
+    if synthetic_type == "Mix":
+        syntheticFlags = [
+            "add_edges",
+            "add_isolated_nodes"
+        ]
+    elif synthetic_type == "Nodes":
+        syntheticFlags = [
+            "add_isolated_nodes"
+        ]
+    elif synthetic_type == "Edges":
+        syntheticFlags = [
+            "add_edges"
+        ]
+
+    for time in range(synthetic_times):
+        syntheticFlag = random.sample(syntheticFlags,1)[0]
+        # randomly choose one network
+        network = random.sample(networks,1)[0]
+        new_network = _synthetic_network(syntheticFlag, network, devices_number)
+
+        # extract com
+        score_set, score_coms = Detection_Outliers(new_network)
+        min_score = sorted(score_set)[0]
+        min_coms = score_coms[min_score]
+
+        # calculate matric
+        # 1. calculate all Recall
+        recallFlag = False
+        for com in min_coms:
+            for node in com:
+                if 'abnormal_device' in node:
+                    recallFlag = True
+                    break
+        if recallFlag is True:
+            Recall.append(1.0)
+        else:
+            Recall.append(0.0)
+
+        # 2. calculate all Precision
+        total_coms = len(min_coms)
+        Precision.append(Recall[-1]/total_coms)
+
+        # 3. calculate F1
+        if Precision[-1] == 0 and Recall[-1] == 0:
+            F1.append(0)
+        else:
+            F1.append(2*Precision[-1]*Recall[-1]/(Precision[-1]+Recall[-1]))
+
+    return Precision, Recall, F1
+
--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import pickle
 import time
 import networkx as nx
 import matplotlib.pyplot as plt
+from statistics import mean
 import Abnormal_Scenario_Generation as ASG
 
-def main(normal_path, abnormal_path):
+def main(normal_path, abnormal_path, synthetic_type):
     """
     main entry
 
@@ -24,24 +25,36 @@ def main(normal_path, abnormal_path):
     # --------------------- #
     # Get Community Score
     # --------------------- #
-    for file in abnormal_files:
-        print('current file:\t', file)
-        # read network
-        network = nx.read_gml(file) # all layer in one. Easy to parse coms
-        # get score
-        score_set, score_com = ASG.Detection_Outliers(network)
+    # for file in abnormal_files:
+    #     print('current file:\t', file)
+    #     # read network
+    #     network = nx.read_gml(file) # all layer in one. Easy to parse coms
+    #     # get score
+    #     score_set, score_com = ASG.Detection_Outliers(network)
 
-        # For DEBUG
-        for s in set(score_set):
-            print('score: ', s)
-            for com in score_com[s]:
-                print('\t',com)
-        print('\n')
-        # END DEBUG
+    #     # For DEBUG
+    #     for s in set(score_set):
+    #         print('score: ', s)
+    #         for com in score_com[s]:
+    #             print('\t',com)
+    #     print('\n')
+    #     # END DEBUG
+
+    # --------------------- #
+    # Get Evaluation
+    # --------------------- #
+    Precision, Recall, F1 = ASG.synthetic_nodes(normal_path,synthetic_type)
+    print('avg Precision:\t', mean(Precision))
+    print('avg Recall:\t', mean(Recall))
+    print('avg F1:\t', mean(F1))
 
 if __name__ == "__main__":
 
     normal_path = 'Normal_Example'
     abnormal_path = 'Abnormal_Example'
 
-    main(normal_path, abnormal_path)
+    # synthetic_type = "Nodes"
+    # synthetic_type = "Edges"
+    synthetic_type = "Mix"
+
+    main(normal_path, abnormal_path, synthetic_type)
